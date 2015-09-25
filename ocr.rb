1a5f4bc5af435440d251e754ad2d8dd4cfea0817
Neil Armstrong <neil.armstrong@sage.com>
2015-09-25 08:34:10 +0100

    rename method

--- ocr.rb
+++ ocr.rb
@@ -2,8 +2,6 @@ def parse_string
 
   #read the input file and split into an array of account numbers
   input = IO.read("input.txt").gsub("\n","").scan(/.{108}/)
-  valid = ""
-  invalid = ""
   output = ""
 
   mappings = { "_| ||_|" => "0",
@@ -20,37 +18,34 @@ def parse_string
   input.each do |string|
     string.gsub!('\n', '')
     parsed_number = ""
-    
+
     for i in [1,4,7,10,13,16,19,22,25]
       number_string = ""
       number_string << string[i]
       number_string << string[i+26..i+28]
       number_string << string[i+53..i+55]
 
-      parsed_number << mappings[number_string]
-    end
+      mappings[number_string] ? parsed_number << mappings[number_string] : parsed_number << "?"
 
-    if valid_number?(parsed_number)
-      valid << "#{parsed_number} \n"
-    else
-      invalid << "#{parsed_number} \n"
     end
-    output = "Valid:\n#{valid}\nInvalid:\n#{invalid}"
+    output << parsed_number
+    output << status(parsed_number)
+    output << "\n"
   end
 
   puts output
 end
 
-def valid_number?(number)
+def status(number)
   reversed = number.reverse
   result = 0
-  for i in 1..9
-    result += reversed[i-1].to_i * i
-  end
-  if (result % 11 == 0)
-    return true
-  else
-    return false
+  if reversed.include?('?')
+    return " ILL"
+  elsif
+    for i in 1..9
+      result += reversed[i-1].to_i * i
+    end
+    result % 11 == 0 ? "" : " ERR"
   end
 end
 
